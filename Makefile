3056700927fb4c90c65ced49d39ff701399ff7ce
k-kashapov <vanyasik247@gmail.com>
2022-03-09 18:17:31 +0300

    %c works

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: 	obj exe
 
 obj: 	Printf.s
-	nasm Printf.s -l NasmLst -f elf64
+	nasm Printf.s -l Listing.lst -f elf64
 
 exe: 	Printf.o
 	ld -s Printf.o -m elf_x86_64 -o Printf.exe
--- Printf.s
+++ Printf.s
@@ -93,36 +93,93 @@ PrintStr:
 ;==============================================
 
 Printf:
-	mov rsi, [rsp + 8]	; load format string to rsi
-	mov rdi, rsi		; rdi - string iterator
-	xor rdx, rdx		; rdx - string lenth counter
-	mov rbx, 1		; rbx - argument iterator
+	mov rsi, [rsp + 8]		; load format string to rsi
+	mov rdi, rsi			; rdi - string iterator
+	xor rdx, rdx			; rdx - string lenth counter
+	mov rbx, 1			; rbx - argument iterator
 
 .loop:
 	xor rax, rax
-	mov al, [rdi] 		; load byte symbols into al
-	inc rdi 		; move to the next symbol
+	mov al, [rdi] 			; load byte symbols into al
+	inc rdi 			; move to the next symbol
 
-	cmp al, '%' 		; if (*Msg == '%')
-	je  .percent		; 	process the % symbol
-				; else
-	cmp al, EOL 		; if str ended
-	je  .end 		; 	end the program
-				; else
-	inc rdx			; 	increment current string length
-	jmp .loop		; process next char
+	cmp al, '%' 			; if (*Msg == '%')
+	je  .percent			; 	process the % symbol
+					; else
+	cmp al, EOL 			; if str ended
+	je  .end 			; 	end the program
+					; else
+	inc rdx				; 	increment current string length
+	jmp .loop			; process next char
 
 .percent:
-	call PrintStrN		; print the whole str before % sym
+	call PrintStrN			; print the whole str before % sym
 	
 	xor rax, rax
-	mov al, [rdi] 		; load next char after % into al
+	mov al, [rdi] 			; load next char after % into al
 	
-	sub al, 'b' 		; rax = letter offset from 'b'
+	sub al, 'b' 			; rax = letter offset from 'b'
 
-	mov rax, [.specSym + rax * 8] 	; jump at the respective char value
-	jmp rax
+	mov rax, [.specSym + rax * 8] 	; load jmp label from jump table
+	jmp rax				; jump at the respective char value
+
+.symS: 					; %s = print string
+        inc rbx				; increment argument counter
+	push qword [rsp + rbx * 8] 	; push next argument = string ptr
+	call PrintStr 			; Print the string
+
+	add rdx, 2			; step over '%s'
+
+	jmp .symEnd
 	
+.symC: 					; %c = print char
+	push rsi			; save rsi
+
+	inc rbx				; inc arg counter
+	mov rsi, [rsp + rbx * 8 + 8] 	; rsi = &char arg
+	
+	push rdx			; save rdx
+	
+	mov rax, 0x01 			; write (rdi, rsi, rdx)
+	mov rdi, 0x01 			; stdout
+	mov rdx, 1 			; write 1 byte
+	syscall
+
+	pop rdx				; restore rdx
+	pop rsi				; restore rsi
+
+	add rdx, 2			; step over '%c'
+	jmp .symEnd
+	
+.symB:
+        mov rax, 1
+.symD:
+        mov rax, 3
+.symO:
+        mov rax, 4
+.symX:
+        mov rax, 6
+
+.otherSym:
+	call PrintStrN
+
+.symEnd:
+	add rsi, rdx		; move string start to the new position
+	
+	mov rdi, rsi		; update string iterator
+	xor rdx, rdx		; reset string len counter
+
+	jmp  .loop
+
+.end:
+	call PrintStrN		; print the remains
+
+	ret
+
+;##############################################
+; Jump table for symbols: b, c, d, o, s
+;##############################################
+
 .specSym:
 	dq .symB
 	dq .symC
@@ -148,57 +205,15 @@ Printf:
         dq .otherSym
         dq .symX
 
-.symS:
-        inc rbx				; increment argument counter
-	push qword [rsp + rbx * 8] 	; push next argument = string ptr
-	call PrintStr 			; Print the string
-
-	add rdx, 2			; step over '%s'
-
-	jmp .symEnd
-	
-.symB:
-        mov rax, 1
-.symC:
-        mov rax, 2
-.symD:
-        mov rax, 3
-.symO:
-        mov rax, 4
-.symX:
-        mov rax, 6
-
-.otherSym:
-	call PrintStrN
-
-.symEnd:
-	add rsi, rdx		; move string start to the new position
-	
-	mov rdi, rsi		; update string iterator
-	xor rdx, rdx		; reset string len counter
-
-	jmp  .loop
-
-.end:
-	call PrintStrN		; print the remains
-
-	ret
-
 ;##############################################
 ; Main
 ;##############################################
 
 _start:
-	push Msg1
-	push Msg1
-	push Msg1
-	push Msg1
+	push Chr
 	push Msg
 	
 	call Printf
-		
-	; mov  rsi, Msg
-	; call PrintStr
 
 	mov rax, 0x3c	; exit (rdi)
 	xor rdi, rdi
@@ -206,5 +221,5 @@ _start:
 
 section .data
 
-Msg	db 'Hello %s %s %s w%sorld', 10, EOL
-Msg1	db 'J%sOJO', EOL
+Msg	db 'Hello %c world', 10, EOL
+Chr	db 'd'
